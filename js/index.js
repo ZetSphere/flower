e1657f985a0ca0d46a5f41c10da2da96cbe48888
Zamzami Ahmad <zammlonewolf@gmail.com>
2025-05-02 17:24:01 -0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,14 +1,14 @@
-const title = document.querySelector('.title')
-const text = 'I Have Something'.split('')
+const title = document.querySelector(".title");
+const text = "何か持っている".split("");
 for (let index = 0; index < text.length; index++) {
-  if (text[index] !== ' ') {
-    title.innerHTML += `<span>${text[index]}<span/>`
+  if (text[index] !== " ") {
+    title.innerHTML += `<span>${text[index]}<span/>`;
   } else {
-    title.innerHTML += `<span style='margin-right: 20px;'><span/>`
+    title.innerHTML += `<span style='margin-right: 20px;'><span/>`;
   }
 }
 
-const textElements = document.querySelectorAll('.title span');
+const textElements = document.querySelectorAll(".title span");
 textElements.forEach((element) => {
   const randomDelay = Math.random() * 3; // Menghasilkan delay acak antara 0 hingga 3 detik
   element.style.animationDelay = `${randomDelay}s`;
