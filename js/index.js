37d5a4d5d040c023a25d0d4f972daf21916a633b
Zamzami Ahmad <zammlonewolf@gmail.com>
2025-08-12 22:39:23 -0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector(".title");
-const text = "何か持っている".split("");
+const text = "For Matchaa".split("");
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== " ") {
     title.innerHTML += `<span>${text[index]}<span/>`;
