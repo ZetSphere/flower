990e9fc43aa1c93851f93443cc423532d02d2d26
Zamzami Ahmad <zamzamiahmd2112@gmail.com>
2026-07-18 17:45:25 +0700

    change index.html, index.js, and main.js

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = "Happy Birthday Elsaa🤍".split("");
+    const titles = "Happy Birthday".split("");
     const titleElement = document.getElementById("title");
     let index = 0;
 
